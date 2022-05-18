19d09b0bad9d06dcf3da6f6fda3bb4b9d469adb6
A. Karl Kornel <akkornel@stanford.edu>
2022-05-17 23:13:32 -0700

    webapp: Start login & logout
    Start handling login and logout.  Logging in works via /login to
    redirect to Globus Auth, and /login/complete to handle the response
    (which ultimately gets saved to the session).  A random state value is
    stored in the session, and checked against the state returned to us when
    /login/complete is called.  This check is important, as it's one part of
    CSRF prevention.
    
    NOTE: When testing locally, be sure to set the `SERVER_NAME` environment
    variable to `localhost`, and to use `localhost` in the URL (instead of
    127.0.0.1).  The redirect URI must be an approved one (that is, listed
    in the Globus Auth client's list of allowed redirect URIs).  It is also
    checked by Globus Auth when we exchange for tokens (another bit of CSRF
    prevention).
    
    Login supports providing a post-login redirect URL via session.
    
    Logout works via /logout to invalidate tokens and redirect back to the
    home page.

--- src/webapp/__init__.py
+++ src/webapp/__init__.py
@@ -31,6 +31,47 @@ from ggm.environ import config
 # Create our Flask webapp
 app = Flask(__name__)
 
+# Create JSON handlers for GlobusUserClients
+class JSONEncoder(flask.json.JSONEncoder):
+    def default(
+        self,
+        o: Any
+    ) -> Any:
+        # We catch all GlobusUserClients instances, converting them to dict
+        if isinstance(o, GlobusUserClients):
+            result = o.to_dict()
+            result['_type'] = 'GlobusUserClients'
+            return result
+        else:
+            return super().default(o)
+class JSONDecoder(flask.json.JSONDecoder):
+    # We set ourselves up by inserting a custom object hook, called after JSON
+    # object are deserialized.  We also make a note of any existing hook.
+    def __init__(self, **kwargs):
+        # Capture any parent object hook
+        self.parent_object_hook = None
+        if 'object_hook' in kwargs:
+            self.parent_object_hook = kwargs['object_hook']
+            del kwargs['object_hook']
+        super().__init__(
+            object_hook=self.decode_dict,
+            **kwargs
+        )
+    # On every dict, if we recognize it, decode it.  Else passs to parent hook.
+    def decode_dict(
+        self,
+        dct: dict[Any, Any],
+    ) -> Any:
+        if '_type' in dct and dct['_type'] == 'GlobusUserClients':
+            dct = cast(GlobusUserClientsDict, dct)
+            return GlobusUserClients.from_dict(dct)
+        else:
+            if self.parent_object_hook is not None:
+                return self.parent_object_hook(dct)
+# Install the JSON encoder & decoder
+app.json_encoder = JSONEncoder
+app.json_decoder = JSONDecoder
+
 # Set up logging
 logger = app.logger
 debug = logger.debug
@@ -52,3 +93,77 @@ except ValueError:
 @app.route('/')
 def hello():
     return "Hello!"
+
+@app.route('/login', methods=['GET'])
+def login_begin():
+    # Generate a secret value for state, and store it in session
+    state = secrets.token_urlsafe()
+    session['login_state'] = state
+
+    # Get our /login/complete URL
+    redirect_url = url_for('login_end', _external=True)
+
+    # Send the user to the URL
+    return redirect(GlobusUserClients.login_url(
+        redirect_uri=redirect_url,
+        renewable=False,
+        state=state,
+    ))
+
+
+@app.route('/login/complete', methods=['GET'])
+def login_end():
+    # Make sure we have both a code and state
+    if 'code' not in request.args:
+        warning('Missing code')
+        abort(400)
+    if 'state' not in request.args:
+        warning('Missing state from URL')
+        abort(400)
+    if 'login_state' not in session:
+        warning('Missing state from session')
+        abort(400)
+
+    # Pull the state from the session
+    session_state = session['login_state']
+    del session['login_state']
+
+    # Make sure our session state matches the request state
+    if session_state != request.args['state']:
+        warning('Session state does not match URL state')
+        abort(401)
+
+    # Get our /login/complete URL
+    redirect_url = url_for('login_end', _external=True)
+
+    # Exchange code for tokens
+    try:
+        clients = GlobusUserClients.from_auth_code(
+            code=request.args['code'],
+            redirect_uri=redirect_url,
+            renewable=False,
+        )
+    except Exception:
+        exception('Error exchanging code for tokens')
+        abort(401)
+    session['clients'] = clients
+
+    # Send the user along!
+    if 'login_url' in session:
+        login_url = session['login_url']
+        del session['login_url']
+    else:
+        login_url = url_for('hello', _external=True)
+    return redirect(login_url)
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    # Do we have any clients?
+    if 'clients' in session:
+        info(f"Logout for {session['clients'].username}")
+        session['clients'].logout()
+        del session['clients']
+    else:
+        debug('Logout called for no user')
+    return redirect(url_for('hello'))
